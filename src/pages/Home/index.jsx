a01b35c85b2e5d0d9e42e70950e915e9f487e798
João Bandeira <joaoml.victor13@gmail.com>
2023-05-10 01:04:20 -0300

    instalacao next ui

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,60 @@
 import "./index.scss";
+import { Navbar, Button, Link, Text } from "@nextui-org/react";
 
 export default function FirstPage() {
+  const collapseItems = [
+    "Features",
+    "Customers",
+    "Pricing",
+    "Company",
+    "Legal",
+    "Team",
+    "Help & Feedback",
+    "Login",
+    "Sign Up",
+  ];
 
   return (
-    <div className="App">
-      aaaaaaaaaaaaaaaaaaaa
-      <input type="button"></input>
-    </div>
+    <Navbar isBordered variant="floating">
+      <Navbar.Brand>
+        <Navbar.Toggle aria-label="toggle navigation" />
+        <Text b color="inherit" hideIn="xs">
+          ACME
+        </Text>
+      </Navbar.Brand>
+      <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
+        <Navbar.Link href="#">Features</Navbar.Link>
+        <Navbar.Link isActive href="#">
+          Customers
+        </Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Company</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Link color="inherit" href="#">
+          Login
+        </Navbar.Link>
+        <Navbar.Item>
+          <Button auto flat as={Link} href="#">
+            Sign Up
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content>
+      <Navbar.Collapse>
+        {collapseItems.map((item, index) => (
+          <Navbar.CollapseItem key={item}>
+            <Link
+              color="inherit"
+              css={{
+                minWidth: "100%",
+              }}
+              href="#"
+            >
+              {item}
+            </Link>
+          </Navbar.CollapseItem>
+        ))}
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
